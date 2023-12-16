d89d1efc042c9de5908c213e3b10cb9be5e70acc
Matt Turner <matt@fiverings.uk>
2023-12-16 16:57:35 +0000

    Improve output of install script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 
 # expects the repository to be cloned within the homeassistant directory
 
-cp ./custom_components/salusfy/*.* ../custom_components/salusfy
+echo "Copying all files from the cloned repo to the Home Assistant custom_components directory..."
+
+cp --verbose ./custom_components/salusfy/*.* ../custom_components/salusfy
+
+echo "Restart Home Assistant to apply the changes"
