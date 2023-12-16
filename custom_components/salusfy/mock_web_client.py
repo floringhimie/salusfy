868de9c0ef1a2e534f2db2f2bfe87f46eb26cc39
Matt Turner <matt@fiverings.uk>
2023-12-16 16:52:11 +0000

    Reuse cached token for 10 mins

--- custom_components/salusfy/mock_web_client.py
+++ custom_components/salusfy/mock_web_client.py
@@ -27,17 +27,21 @@ class MockWebClient:
 
     def set_temperature(self, temperature):
         """Set new target temperature."""
+        
+        _LOGGER.info("Setting temperature to %.1f...", temperature)
+        
         self._state.target_temperature = temperature
-        _LOGGER.info("Salusfy set_temperature OK")
 
 
     def set_hvac_mode(self, hvac_mode):
         """Set HVAC mode, via URL commands."""
+        
+        _LOGGER.info("Setting the HVAC mode to %s...", hvac_mode)
+
         if hvac_mode == HVAC_MODE_OFF:
             self._state.current_operation_mode = STATE_OFF
         elif hvac_mode == HVAC_MODE_HEAT:
             self._state.current_operation_mode = STATE_ON
-        _LOGGER.info("Setting the HVAC mode.")
 
 
     def get_state(self):
