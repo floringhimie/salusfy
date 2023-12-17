df0935b236b5c276f3b52170f0237a41a7e9e510
Matt Turner <matt@fiverings.uk>
2023-12-17 15:57:30 +0000

    Verify salus only called once

--- tests/test_thermostat_entity.py
+++ tests/test_thermostat_entity.py
@@ -2,29 +2,72 @@ import pytest
 from unittest.mock import MagicMock
 
 from salusfy import ( ThermostatEntity, State )
+from homeassistant.components.climate.const import (
+    HVAC_MODE_HEAT
+)
 
-
-def test_entity_is_registered():
-    mock_client = MagicMock()
-    mock_ha_client = MagicMock()
+@pytest.fixture
+def mock_client():
+    mock = MagicMock()
 
     state = State()
     state.current_temperature = 15.3
     state.target_temperature = 33.3
-    mock_client.configure_mock(
+    mock.configure_mock(
         **{
             "get_state.return_value": state
         }
     )
 
-    mock_ha_client.configure_mock(
+    return mock
+
+@pytest.fixture
+def mock_ha_client():
+    mock = MagicMock()
+
+    mock.configure_mock(
         **{
             "current_temperature.return_value": 21.1
         }
     )
-
-    target = ThermostatEntity('mock', mock_client, mock_ha_client)
     
-    assert target.current_temperature == 21.1
+    return mock
+
+def test_entity_returns_target_temp_from_web_client(mock_client, mock_ha_client):
+    target = ThermostatEntity('mock', mock_client, mock_ha_client)
+
     assert target.target_temperature == 33.3
-    # mock_table.insert.assert_called_once()
+
+
+def test_entity_returns_target_temp_from_home_assistant_client(mock_client, mock_ha_client):
+    target = ThermostatEntity('mock', mock_client, mock_ha_client)
+
+    assert target.current_temperature == 21.1
+
+
+def test_entity_call_salus_client_only_once(mock_client, mock_ha_client):
+    target = ThermostatEntity('mock', mock_client, mock_ha_client)
+
+    target.update()
+    target.update()
+
+    mock_client.get_state.assert_called_once()
+    assert target.target_temperature == 33.3
+
+
+def test_entity_delegates_set_temperature_salus_client(mock_client, mock_ha_client):
+    target = ThermostatEntity('mock', mock_client, mock_ha_client)
+
+    target.set_temperature(temperature=29.9)
+
+    mock_client.set_temperature.assert_called_once_with(29.9)
+    assert target.target_temperature == 29.9
+
+
+def test_entity_delegates_set_hvac_mode_to_salus_client(mock_client, mock_ha_client):
+    target = ThermostatEntity('mock', mock_client, mock_ha_client)
+
+    target.set_hvac_mode(hvac_mode=HVAC_MODE_HEAT)
+
+    mock_client.set_hvac_mode.assert_called_once_with(HVAC_MODE_HEAT)
+    assert target.hvac_mode == HVAC_MODE_HEAT
